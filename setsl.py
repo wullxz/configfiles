0f71f91c032bff337e2fd427dff543e51cad453e
Meik Specht <meik@schnitzelspecht.de>
2015-09-01 15:12:48 +0200

    added -a|--assumeyes option for non-interactive use

--- setsl.py
+++ setsl.py
@@ -2,6 +2,18 @@
 
 from subprocess import call
 import os.path
+import sys
+
+assumeyes = False
+
+if (len(sys.argv) > 1):
+  if (len(sys.argv) == 2):
+    if (sys.argv[1] == "--assumeyes" or sys.argv[1] == "-a"):
+      assumeyes = True
+  else:
+    print "Usage: setsl.py [-a|--assumeyes]"
+    print "\t-a|--assumeyes: assumes 'yes' for all questions"
+
 
 fileasoc = [
   ["vimrc", "~/.vimrc"],
@@ -21,16 +33,20 @@ for f in fileasoc:
 
     # link or file already exists! ask if it should be replaced
   if (os.path.exists(source) or os.path.islink(source)):
-    choice = raw_input("File %s already exists! Override? [Y/n] " % source).lower()
-    if choice in yes:
+    if (assumeyes):
       call(["rm", source])
       override = True
-    elif choice in no:
-      override = False
-      print "Not overriding %s! Skipping..." % source
-      continue
     else:
-      print "Please respond with 'yes' or 'no'! Skipping %s for now..." % source
+      choice = raw_input("File %s already exists! Override? [Y/n] " % source).lower()
+      if choice in yes:
+        call(["rm", source])
+        override = True
+      elif choice in no:
+        override = False
+        print "Not overriding %s! Skipping..." % source
+        continue
+      else:
+        print "Please respond with 'yes' or 'no'! Skipping %s for now..." % source
   else:
     print "File %s not existing... doing symlink now!" % source
 
